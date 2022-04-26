d33594ff23b80d116b0702c68b015535533ab03a
User <mekendemin@gmail.com>
2022-04-26 19:08:51 +0600

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        for (int i = 5; i <= 5; i++) {
+            for (int n = 1; n <= 10; n++) {
+                System.out.println(i + "*" + n + "=" + (n* i) + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
